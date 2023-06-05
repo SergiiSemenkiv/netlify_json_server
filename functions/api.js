38c507f267db7307cce683a8673b826654392dd1
sergii_semenkiv <ssv260689@gmail.com>
2023-06-05 12:25:10 +0300

    Added netlify function

---
+++ functions/api.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const dataFilePath = '../data.json';
+
+let data = require(dataFilePath);
+
+exports.handler = async (event, context) => {
+  const { path, httpMethod, body } = event;
+
+  if (path === '/users') {
+    if (httpMethod === 'GET') {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data.users)
+      };
+    } else if (httpMethod === 'POST') {
+      const newUser = JSON.parse(body);
+      const newId = data.users.length + 1;
+      newUser.id = newId;
+      data.users.push(newUser);
+      
+      fs.writeFileSync(dataFilePath, JSON.stringify(data));
+      
+      return {
+        statusCode: 200,
+        body: JSON.stringify(newUser)
+      };
+    }
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: 'Route not found' })
+    };
+  }
+};
