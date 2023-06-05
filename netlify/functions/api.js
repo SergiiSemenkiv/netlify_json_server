d38eeb9cf9899fba911591e02043f11d3493b2db
sergii_semenkiv <ssv260689@gmail.com>
2023-06-05 12:42:06 +0300

    switched to process cwd

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const dataFilePath = path.join(__dirname, 'data.json');
+const dataFilePath = path.resolve(process.cwd(), 'data.json');
 
 let data = require(dataFilePath);
 
